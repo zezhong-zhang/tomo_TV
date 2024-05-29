fd5280ed5ec43cd32d231673743969de7a915786
Zezhong <jack.zezhong.zhang@gmail.com>
2024-05-29 17:59:47 +0200

    compiled

--- setup_tomo_tv.sh
+++ setup_tomo_tv.sh
@@ -1,5 +1,5 @@
-export LD_LIBRARY_PATH=/path/to/tomo_TV/thirdparty/astra/lib:$LD_LIBRARY_PATH
-export LD_LIBRARY_PATH=/path/to/tomo_TV/gpu_3D/Utils:$LD_LIBRARY_PATH
-export PYTHONPATH=$PYTHONPATH:/path/to/tomo_TV/thirdparty/astra/lib/python3/site-packages/
-export PYTHONPATH=$PYTHONPATH:/path/to/tomo_TV/gpu_3D/Utils
+export LD_LIBRARY_PATH=/home/zzhang/OneDrive/code/tomo_TV/thirdparty/astra/lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=/home/zzhang/OneDrive/code/tomo_TV/gpu_3D/Utils:$LD_LIBRARY_PATH
+# export PYTHONPATH=$PYTHONPATH:/home/zzhang/OneDrive/code/tomo_TV/thirdparty/astra/lib/python3/site-packages/
+export PYTHONPATH=$PYTHONPATH:/home/zzhang/OneDrive/code/tomo_TV/gpu_3D/Utils
 
